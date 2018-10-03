761a254cad595ec73a17761becc2f6d8e112ae96
Ofir Smuha <ofir@500tech.com>
2018-10-03 11:14:22 +0300

    re-doing dates with lodash/fp

--- proj/src/components/Header.js
+++ proj/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import moment from 'moment'
+import {head, last} from 'lodash/fp'
+
 
 export default (props) => {
+
+  const startDate = moment(head(props.times)).format('MMM DD, YYYY')
+  const endDate = moment(last(props.times)).format('MMM DD, YYYY');
+
+
   return (
     <div className="header-container">
       <div>
         <h1 className="title">{props.children}</h1>
-        <h3 className="dates">{moment(props.times[0]).format('MMM DD, YYYY')} - {moment(props.times[6]).format('MMM DD, YYYY')}</h3>
+        <h3 className="dates">{startDate} - {endDate}</h3>
       </div>
       <div className="scroll-btns">
         <button onClick={() => props.updateOffset(-7)} className="prev-btn">PREVIOUS</button>
