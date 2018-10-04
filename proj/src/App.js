4b088b67ca2de771e7d40bfc276bf6c084ffd98b
Ofir Smuha <ofir@500tech.com>
2018-10-04 17:41:50 +0300

    router setup + loging design

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,68 +1,12 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import TimesList from './components/TimesList';
-import moment from 'moment';
-import {set} from 'lodash/fp'
+import React, { Component } from 'react'
+import AppRouter from './routes/AppRouter'
 
 class App extends Component {
-  state = {
-    offset: 0,
-    dates: [],
-    hours: {}
-  };
-
-  componentDidMount() {
-    this.renderDates();
-  }
-
-  changeTime = (hoursValue, id) => {
-    const hours = { ...this.state.hours };
-
-    this.setState({
-      hours: set([id], hoursValue, hours)
-    })
-  }
-
-  updateOffset = (offset) => {
-    this.setState({
-      offset: this.state.offset + offset
-    }, this.renderDates)
-  }
-
-  renderDates = () => {
-    const today = moment().add(this.state.offset, 'days');
-    const dates = [];
-
-    for (let i = 0; i < 7; i++) {
-      const date = today.clone().add(i, 'days').format('MM.DD.YY')
-
-      dates.push({
-        id : date,
-      });
-    }
-
-    this.setState({
-      dates
-    });
-    
-  }
-
   render() {
     return (
-      <div className="App">
-        <Header 
-          dates={this.state.dates}
-          updateOffset={this.updateOffset}>
-          Time Tracking
-        </Header>
-        <TimesList 
-          dates={this.state.dates}
-          changeTime={this.changeTime}
-          hours={ this.state.hours }
-        />
-      </div>
-    );
+      <AppRouter/>
+    )
   }
 }
 
-export default App;
+export default App
