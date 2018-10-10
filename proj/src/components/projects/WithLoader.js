e90a2b1bd15c54affb7dd70dec1bcd009c9b1c61
Ofir Smuha <ofir@500tech.com>
2018-10-10 11:11:50 +0300

    ring-loader SVG & withLoader component

---
+++ proj/src/components/projects/WithLoader.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import loader from 'assets/images/ring-loader.gif'
+
+const Loader = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%; 
+  transform: translate(-50%, -50%);
+  width: 80px;
+`
+
+
+const WithLoader = (WrappedComponent) => {
+  return class extends Component {
+    state = {}
+    
+    render() {
+        if (this.props.isLoading) return (<Loader src={loader} alt="loading"/>)
+        return (<WrappedComponent {...this.props}></WrappedComponent>)
+    }
+  }
+}
+
+export default WithLoader
